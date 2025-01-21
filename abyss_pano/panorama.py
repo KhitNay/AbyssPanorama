d94603592346c709ce9c9b0bee7da0b509704301
Khit Nay <khitnay.adrian@gmail.com>
2025-01-22 00:25:00 +1100

    use cv bridge

--- abyss_pano/panorama.py
+++ abyss_pano/panorama.py
@@ -13,9 +13,16 @@ class Panorama(Node):
                                                    '/platypus/camera_2/dec/manual_white_balance',
                                                    self.image_callback,
                                                    10)
+        self.cv_bridge = CvBridge()
 
     def image_callback(self, msg):
-        self.get_logger().info("Image received from camera 2")
+        self.get_logger().info("Received image")
+        try:
+            cv_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+            cv2.imshow('Middle Camera', cv_image)
+            cv2.waitKey(1)
+        except Exception as e:
+            self.get_logger().error('Could not convert image: {}'.format(e))
 
 
 def main(args=None):
